403c4632b118ae94f4fcc56aa2cd5d86d25ea3cf
PhietroSolonoo <negociosphietro@gmail.com>
2026-05-19 19:54:23 -0300

    feat: adiciona todos os Response DTOs

---
+++ src/main/java/br/com/fiap/clyvovet/dto/response/AlertaSaudeResponse.java
@@ -0,0 +1,15 @@
+package br.com.fiap.clyvovet.dto.response;
+
+import java.time.LocalDateTime;
+import br.com.fiap.clyvovet.enums.TipoAlerta;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlertaSaudeResponse {
+    private Long id;
+
+}
