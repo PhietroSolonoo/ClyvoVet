1d0a9f9451106173257f8d6df88e96d1cdaa3d06
PhietroSolonoo <negociosphietro@gmail.com>
2026-05-19 15:09:48 -0300

    feat: estrutura inicial do projeto e suas dependências

---
+++ src/main/java/br/com/fiap/clyvovet/ClyvovetApplication.java
@@ -0,0 +1,14 @@
+package br.com.fiap.clyvovet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClyvovetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClyvovetApplication.class, args);
+        System.out.println("ClyvoVet API is running!");
+        System.out.println("Swagger UI: http://localhost:8080/swagger-ui.html");
+    }
+}
